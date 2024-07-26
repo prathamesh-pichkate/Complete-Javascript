5bb92783e4d4059abe408b2de9ba6cdce2a152f7
Prathamesh Pichkate <prathameshpichkate@gmail.com>
2024-07-26 09:01:52 +0000

    dataconversion file

---
+++ 0.1Basics/3.DataConversion.js
@@ -0,0 +1,19 @@
+let name = "prathamesh";
+let convert = Number(name);
+// console.table([typeof name, typeof convert]);
+
+let num = 23654;
+let convertStr = String(num)
+// console.table([typeof num, typeof convertStr]);
+
+
+// Check the conversion for boolean
+let n = 1
+let nCon = Boolean(n)
+let empty = "" //Written true
+let zero = 0  //written false
+let str = "name" //written true
+
+console.log(empty)
+console.log(nCon)
+console.table([typeof n,typeof nCon])
